9a3ec6f3ae5f21ab8b219ec23e0a6094ad45a480
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-09-13 23:39:24 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Sidebar from './component/Sidebar/Sidebar';
+import MainDash from './component/MainDash/MainDash';
+import RightSide from './component/RigtSide/RigtSide'
+
+function App() {
+  return (
+    <div className="App">
+     <div className='AppGlass'>
+      <Sidebar/>
+      <MainDash/>
+      <RightSide/>
+     </div>
+    </div>
+  );
+}
+// udpated
+// updated
+export default App;
